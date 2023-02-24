ab0bf76543223fd843557be602041caffa114ba1
ann <dorisann.me@gmail.com>
2023-02-24 22:36:51 +0000

    uncommented my lines of code

--- prompt.py
+++ prompt.py
@@ -1,29 +1,29 @@
 # Write 5 prompt statements to ask about favorite word, second favorite word, favorite personality
 # in the world, name of pets and favorite body parts.
 
-# favWord = input("What is your favorite word?: ")
-# secondFavWord = input("What is your second favorite word?: ")
-# favPersonality = input("What is your favorite personality?: ")
-# nameOfPets = input("What is the name of your pet?: ")
-# favBodyPart = input("What is your favorite body part?: ")
+favWord = input("What is your favorite word?: ")
+secondFavWord = input("What is your second favorite word?: ")
+favPersonality = input("What is your favorite personality?: ")
+nameOfPets = input("What is the name of your pet?: ")
+favBodyPart = input("What is your favorite body part?: ")
 
-# message = "Hello it's been nice knowing you. Your favorite word is " + favWord + " and your second favorite word is " + secondFavWord + " I love your favorite personality which is " + favPersonality + " and your lovely pet name which is " + nameOfPets +  " and finally your favorite body part which is " + favBodyPart
-# print(message)
+message = "Hello it's been nice knowing you. Your favorite word is " + favWord + " and your second favorite word is " + secondFavWord + " I love your favorite personality which is " + favPersonality + " and your lovely pet name which is " + nameOfPets +  " and finally your favorite body part which is " + favBodyPart
+print(message)
 
 
 # prompt a user to indicate the number of books read in a month and based on that deduce the number of books he reads in a year.
-# numberOfBooksRead = input("How many books do you read in a month?: ")
-# answer = int(numberOfBooksRead) * 12
-# print("You will read", answer, "books in a year." )
+numberOfBooksRead = input("How many books do you read in a month?: ")
+answer = int(numberOfBooksRead) * 12
+print("You will read", answer, "books in a year." )
 
 #second
-# numberOfBooksRead = input("How many books do you read in a month?: ")
-# numberOfBooksRead = int(numberOfBooksRead) 
-# print(numberOfBooksRead * 12)
+numberOfBooksRead = input("How many books do you read in a month?: ")
+numberOfBooksRead = int(numberOfBooksRead) 
+print(numberOfBooksRead * 12)
 
 # third
-# numberOfBooksRead = int(input("How many books do you read in a month?: "))
-# print(numberOfBooksRead * 12)
+numberOfBooksRead = int(input("How many books do you read in a month?: "))
+print(numberOfBooksRead * 12)
 
 
 # ASSIGNMENT
