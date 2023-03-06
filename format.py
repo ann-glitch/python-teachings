e6f3aad6da198bf812dc7310bcf570d959736295
ann <dorisann.me@gmail.com>
2023-03-06 20:12:09 +0000

    add: more solutions

--- format.py
+++ format.py
@@ -6,3 +6,26 @@ name = "Sammmy"
 "Hello my dog's name is Sammy and he's 4 years old"
 
 print("Hello my dog's name is {} and he's {} years old.".format(name, age)) 
+
+e = "hello" + "there"
+print(e)
+
+n = 5 # line1
+while n > 0: #line2
+ print(n) #line3
+ n = n - 1 #line4
+print('Blastoff!') #line5
+
+# n = 5
+# while n > 0:
+#  print('Lather')
+#  print('Rinse')
+# print('Dry off!')
+
+print('Searching')
+for name in ['Akua', 'Yaw', 'Esi', 'Efe', 'Ama']:
+    if name == 'Esi':
+        print("Name found:", name)
+    print("Finished")
+
+
