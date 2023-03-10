48ac2db07969c24d424687f4fc5d4dd193214296
ann <dorisann.me@gmail.com>
2023-03-10 22:41:51 +0000

    add: loops

---
+++ loops.py
@@ -0,0 +1,17 @@
+# using atleat 5 ways, show the output 0,1,2,3 using the while and for loop
+
+# a = [0,1,2,3]
+
+for a in range(4):
+    print(a)
+
+# second
+for b in [0,1,2,3]:
+    print(b)
+
+      
+# while
+a = 0
+while a <= 3:
+    print(a)
+    a = a+1
